78cefe3a3f0278b2ec3f86fb1a8c44f2210e3b58
Hiremath <e070769@c1msrdbxh3qf.mastercard.com>
2018-01-31 22:31:02 -0500

    Spring JDBC Template

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
@@ -17,8 +19,8 @@ public class PalTrackerApplication {
     }
 
     @Bean
-    public TimeEntryRepository createTimeEntryRepository(){
-        return new InMemoryTimeEntryRepository();
+    public TimeEntryRepository createTimeEntryRepository(DataSource dataSource){
+        return new JdbcTimeEntryRepository(dataSource);
     }
 
     @Bean
